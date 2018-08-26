dbf2e9afa374d664b24cf5829e0ade495bd3c103
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-08-26 14:19:37 -0400

    styled cleaning options page with color, vertical stacking, zoom on hover. decided on using sweet alert for popup details due to lack of space. weighing whether to put detail code on HTML or JS. HTML pros: better organization, cons: messier code. JS pros: cleaner, simpler code, cons: less organization

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-main',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   activeFormPage = 1;
+  firstDetails = 'Details';
+  secondDetails = 'dets';
+  thirdDetails = 'doods';
 
   constructor() { }
 
@@ -26,6 +30,10 @@ export class MainComponent implements OnInit {
     document.getElementById(`${this.activeFormPage}`).classList.add('faded');
     this.activeFormPage++;
     document.getElementById(`${this.activeFormPage}`).classList.remove('hidden');
+  }
+
+  popUpServiceDetails(details: string){
+    swal(details);
   }
 
 }

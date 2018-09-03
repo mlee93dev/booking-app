675f1e77eeb6a54380791f9a602308d17ac211ed
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-09-02 23:57:39 -0400

    unit test for location sub

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,14 +1,29 @@
 import { MainComponent } from "./main.component";
 import { TimeService } from "./time.service";
 import { SocketService } from './socket.service';
+import { Location } from './models/location.model';
 
 describe('MainComponent', () => {
   let timeService = new TimeService();
   let socketService = new SocketService();
   const comp = new MainComponent(timeService, socketService);
 
-  it('activeFormPage should initialize as 1', () => {
-    expect(comp.activeFormPage).toBe(1);
+  it('location subscription should change data', () => {
+    comp.city = 'Boston';
+    comp.state = 'MA';
+    let idealPlace = {city: 'Seattle', state: 'WA'};
+
+    comp.locationSubscription = socketService.dataReady
+      .subscribe(
+        (locationData: Location) => {
+          comp.city = locationData.city;
+          comp.state = locationData.state;
+        }
+      );
+    socketService.dataReady.next(idealPlace);
+
+    expect(comp.city).toBe(idealPlace.city);
+    expect(comp.state).toBe(idealPlace.state);
   });
-  
+
 })

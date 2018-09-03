fac7b7fc67afbdc73bd5448754bad9955779d52a
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-09-03 14:19:41 -0400

    initial test suite for main component

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -8,7 +8,7 @@ describe('MainComponent', () => {
   let socketService = new SocketService();
   const comp = new MainComponent(timeService, socketService);
 
-  it('location subscription should change data', () => {
+  it('-location subscription should change data', () => {
     comp.city = 'Boston';
     comp.state = 'MA';
     let idealPlace = {city: 'Seattle', state: 'WA'};
@@ -25,5 +25,61 @@ describe('MainComponent', () => {
     expect(comp.city).toBe(idealPlace.city);
     expect(comp.state).toBe(idealPlace.state);
   });
+
+  it('-should correctly set the calendar', () => {
+    comp.defaultDate = new Date('September 1, 2018');
+    comp.setCalendar();
+
+    expect(comp.currentYear).toBe(2018);
+    expect(comp.currentMonth).toBe('September');
+    expect(comp.datesList.length).toBe(30);
+    expect(comp.initialDayOfWeek.length).toBe(6);
+  });
+
+  it('-should correctly set greyed out and non greyed out days', () => {
+    comp.setCalendar();
+    comp.greyOutDaysBeforePresentDay();
+    let datesSum = comp.daysBeforePresentDayList.length + comp.nonGreyedOutDatesList.length;
+    expect(datesSum).toBe(comp.datesList.length);
+  });
+
+  it('-date subscription should change date', () => {
+    comp.defaultDate = new Date('January 1, 2000');
+    comp.setCalendar();
+    expect(comp.currentMonth).toBe('January');
+
+    comp.dateSubscription = timeService.dateChanged
+      .subscribe(
+        (newDate: Date) => {
+          comp.defaultDate = newDate;
+          comp.setCalendar();
+        }
+      );
+    let newDate = new Date('February 1, 2000');
+    timeService.dateChanged.next(newDate);
+    expect(comp.currentMonth).toBe('February');
+  });
+
+  it('-should select correct cleaning option', () => {
+    comp.selectCleaning(3);
+    expect(comp.selectedCleanOption).toBe(3);
+  });
+
+  it('-should correctly toggle clean details', () => {
+    comp.activeCleanDetails = 1;
+    comp.toggleCleanDetails(1);
+    expect(comp.activeCleanDetails).toBe(null);
+    comp.toggleCleanDetails(3);
+    expect(comp.activeCleanDetails).toBe(3);
+  });
+
+  it('-should correctly decrement / increment the month', () => {
+    comp.defaultDate = new Date('January 1, 2000');
+    comp.setCalendar();
+    comp.prevMonth();
+    expect(comp.currentMonth).toBe('December');
+    comp.nextMonth();
+    expect(comp.currentMonth).toBe('January');
+  })
 
 })

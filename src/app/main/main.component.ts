02a34a160684c04a0656fc37ed3e0d944b90c325
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-10-03 16:20:51 -0400

    successfully implemented auth2.disconnect to prevent user to be able to automatically relogin

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -171,20 +171,19 @@ export class MainComponent implements OnInit, AfterViewInit, OnDestroy {
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail());
+        console.log(this.auth2);
         //YOUR CODE HERE
 
 
-      }, (error) => {
-        alert(JSON.stringify(error, undefined, 2));
       });
   }
 
   signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      auth2.disconnect();
-      console.log('User signed out.');
+      console.log('User signed out.', auth2);
     });
+    auth2.disconnect().then(() => console.log('Disconnected.', auth2))
   }
 
 }

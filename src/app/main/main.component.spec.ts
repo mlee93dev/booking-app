0240ccf5ce0d63f8ca7ec67aa61f5d130473e04b
Mark <mark.lee1933@gmail.com>
2018-10-29 12:20:09 -0700

    added back googleSignOut unit test with mock service signout. completed separating DOM emulation unit tests from google service unit tests. uncommented out the previous tests

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -8,137 +8,141 @@ import { GoogleService } from "./google.service";
 import { ComponentFixture, TestBed, async } from "@angular/core/testing";
 import { By } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
-import { DebugElement, getDebugNode } from "@angular/core";
+import { DebugElement } from "@angular/core";
 import { resolve } from "path";
 import { reject } from "q";
 
-// let timeService = new TimeService();
-// let socketService = new SocketService();
-// let googleService = new GoogleService();
-// const comp = new MainComponent(timeService, socketService, googleService);
+describe('MainComponent', () => {
+  let timeService = new TimeService();
+  let socketService = new SocketService();
+  let googleService = new GoogleService();
+  const comp = new MainComponent(timeService, socketService, googleService);
 
-// describe('MainComponent', () => {
-//   it('-location subscription should change data', () => {
-//     comp.city = 'Boston';
-//     comp.state = 'MA';
-//     let idealPlace = {city: 'Seattle', state: 'WA'};
+  it('-location subscription should change data', () => {
+    comp.city = 'Boston';
+    comp.state = 'MA';
+    let idealPlace = {city: 'Seattle', state: 'WA'};
 
-//     comp.locationSubscription = socketService.dataReady
-//       .subscribe(
-//         (locationData: Location) => {
-//           comp.city = locationData.city;
-//           comp.state = locationData.state;
-//         }
-//       );
-//     socketService.dataReady.next(idealPlace);
+    comp.locationSubscription = socketService.dataReady
+      .subscribe(
+        (locationData: Location) => {
+          comp.city = locationData.city;
+          comp.state = locationData.state;
+        }
+      );
+    socketService.dataReady.next(idealPlace);
 
-//     expect(comp.city).toBe(idealPlace.city);
-//     expect(comp.state).toBe(idealPlace.state);
-//   });
+    expect(comp.city).toBe(idealPlace.city);
+    expect(comp.state).toBe(idealPlace.state);
+  });
 
-//   it('-should correctly set the calendar', () => {
-//     comp.defaultDate = new Date('September 1, 2018');
-//     comp.setCalendar();
+  it('-should correctly set the calendar', () => {
+    comp.defaultDate = new Date('September 1, 2018');
+    comp.setCalendar();
 
-//     expect(comp.currentYear).toBe(2018);
-//     expect(comp.currentMonth).toBe('September');
-//     expect(comp.datesList.length).toBe(30);
-//     expect(comp.initialDayOfWeek.length).toBe(6);
-//   });
+    expect(comp.currentYear).toBe(2018);
+    expect(comp.currentMonth).toBe('September');
+    expect(comp.datesList.length).toBe(30);
+    expect(comp.initialDayOfWeek.length).toBe(6);
+  });
 
-//   it('-should correctly set greyed out and non greyed out days', () => {
-//     comp.setCalendar();
-//     comp.greyOutDaysBeforePresentDay();
-//     let datesSum = comp.daysBeforePresentDayList.length + comp.nonGreyedOutDatesList.length;
-//     expect(datesSum).toBe(comp.datesList.length);
-//   });
+  it('-should correctly set greyed out and non greyed out days', () => {
+    comp.setCalendar();
+    comp.greyOutDaysBeforePresentDay();
+    let datesSum = comp.daysBeforePresentDayList.length + comp.nonGreyedOutDatesList.length;
+    expect(datesSum).toBe(comp.datesList.length);
+  });
 
-//   it('-date subscription should change date', () => {
-//     comp.defaultDate = new Date('January 1, 2000');
-//     comp.setCalendar();
-//     expect(comp.currentMonth).toBe('January');
+  it('-date subscription should change date', () => {
+    comp.defaultDate = new Date('January 1, 2000');
+    comp.setCalendar();
+    expect(comp.currentMonth).toBe('January');
 
-//     comp.dateSubscription = timeService.dateChanged
-//       .subscribe(
-//         (newDate: Date) => {
-//           comp.defaultDate = newDate;
-//           comp.setCalendar();
-//         }
-//       );
-//     let newDate = new Date('February 1, 2000');
-//     timeService.dateChanged.next(newDate);
-//     expect(comp.currentMonth).toBe('February');
-//   });
+    comp.dateSubscription = timeService.dateChanged
+      .subscribe(
+        (newDate: Date) => {
+          comp.defaultDate = newDate;
+          comp.setCalendar();
+        }
+      );
+    let newDate = new Date('February 1, 2000');
+    timeService.dateChanged.next(newDate);
+    expect(comp.currentMonth).toBe('February');
+  });
 
-//   it('-should select correct cleaning option', () => {
-//     comp.selectCleaning(3);
-//     expect(comp.selectedCleanOption).toBe(3);
-//   });
+  it('-should select correct cleaning option', () => {
+    comp.selectCleaning(3);
+    expect(comp.selectedCleanOption).toBe(3);
+  });
 
-//   it('-should correctly toggle clean details', () => {
-//     comp.activeCleanDetails = 1;
-//     comp.toggleCleanDetails(1);
-//     expect(comp.activeCleanDetails).toBe(null);
-//     comp.toggleCleanDetails(3);
-//     expect(comp.activeCleanDetails).toBe(3);
-//   });
+  it('-should correctly toggle clean details', () => {
+    comp.activeCleanDetails = 1;
+    comp.toggleCleanDetails(1);
+    expect(comp.activeCleanDetails).toBe(null);
+    comp.toggleCleanDetails(3);
+    expect(comp.activeCleanDetails).toBe(3);
+  });
 
-//   it('-should correctly decrement / increment the month', () => {
-//     comp.defaultDate = new Date('January 1, 2000');
-//     comp.setCalendar();
-//     comp.prevMonth();
-//     expect(comp.currentMonth).toBe('December');
-//     comp.nextMonth();
-//     expect(comp.currentMonth).toBe('January');
-//   });
+  it('-should correctly decrement / increment the month', () => {
+    comp.defaultDate = new Date('January 1, 2000');
+    comp.setCalendar();
+    comp.prevMonth();
+    expect(comp.currentMonth).toBe('December');
+    comp.nextMonth();
+    expect(comp.currentMonth).toBe('January');
+  });
 
-// });
+});
 
-// describe('Socket.IO', () => {
-//   const SERVER_URL = window.location.host;
-//   const mockServer = new Server(SERVER_URL);
-//   let requestedLocation = { city: 'Seattle', state: 'WA' };
+describe('Socket.IO', () => {
+  let timeService = new TimeService();
+  let socketService = new SocketService();
+  let googleService = new GoogleService();
+  const comp = new MainComponent(timeService, socketService, googleService);
+  const SERVER_URL = window.location.host;
+  const mockServer = new Server(SERVER_URL);
+  let requestedLocation = { city: 'Seattle', state: 'WA' };
 
-//   //Mock server socket emitter
-//   mockServer.on('connection', socket => {
-//     setTimeout(() => {
-//       mockServer.emit('sentLocationDetails', requestedLocation);
-//     }, 500);
-//   });
+  //Mock server socket emitter
+  mockServer.on('connection', socket => {
+    setTimeout(() => {
+      mockServer.emit('sentLocationDetails', requestedLocation);
+    }, 500);
+  });
 
-//   beforeEach(() => {
-//     jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
-//   });
+  beforeEach(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+  });
 
-//   //Mock client socket receiver
-//   it('-ioConnection should receive location from socket observable', async (done) => {
-//     (window as any).io = SocketIO;
-//     let socket = io(window.location.host);
-//     let receivedLocation: Location;
+  //Mock client socket receiver
+  it('-ioConnection should receive location from socket observable', async (done) => {
+    (window as any).io = SocketIO;
+    let socket = io(window.location.host);
+    let receivedLocation: Location;
 
-//     function onLocationReceived(): Observable<Location> {
-//       return new Observable<Location>((observer) => {
-//         socket.on('sentLocationDetails', (locationData: Location) => observer.next(locationData));
-//       });
-//     }
+    function onLocationReceived(): Observable<Location> {
+      return new Observable<Location>((observer) => {
+        socket.on('sentLocationDetails', (locationData: Location) => observer.next(locationData));
+      });
+    }
 
-//     comp.initIoConnection = function() {
-//       this.ioConnection = onLocationReceived()
-//         .subscribe(
-//           {next(locationData: Location) {
-//               receivedLocation = locationData;
-//               expect(receivedLocation.city).toBe(requestedLocation.city);
-//               expect(receivedLocation.state).toBe(requestedLocation.state);
-//               mockServer.stop();
-//               done();
-//             }
-//           }
-//         );
-//     }
+    comp.initIoConnection = function() {
+      this.ioConnection = onLocationReceived()
+        .subscribe(
+          {next(locationData: Location) {
+              receivedLocation = locationData;
+              expect(receivedLocation.city).toBe(requestedLocation.city);
+              expect(receivedLocation.state).toBe(requestedLocation.state);
+              mockServer.stop();
+              done();
+            }
+          }
+        );
+    }
 
-//     comp.initIoConnection();
-//   });
-// });
+    comp.initIoConnection();
+  });
+});
 
 describe('GoogleBtn and DOM Emulation', () => {
   let fixture: ComponentFixture<MainComponent>;
@@ -219,7 +223,7 @@ describe('GoogleService Methods', () => {
   let timeService = new TimeService();
   let socketService = new SocketService();
   let googleService = new GoogleService();
-  let comp = new MainComponent(timeService, socketService, googleService);
+  const comp = new MainComponent(timeService, socketService, googleService);
 
   let success = true;
   let auth2 = 'dummyauth';
@@ -259,21 +263,23 @@ describe('GoogleService Methods', () => {
       .catch((e) => expect(e).toBe(errormsg));
   });
 
-  // it('-googleAttachSignIn() should catch appropriate response', () => {
-  //   console.log(googleService.auth2);
-  //   success = true;
-  //   component.googleAttachSignIn(dummyElement)
-  //     .then((status) => expect(status).toBe('Attached.'));
+  it('-googleAttachSignIn() should catch appropriate response', () => {
+    success = true;
+    comp.googleAttachSignIn(dummyElement)
+      .then((status) => expect(status).toBe('Attached.'));
 
-  //   success = false;
-  //   component.googleAttachSignIn(dummyElement)
-  //     .catch((error) => expect(error).toBe(errormsg));
-  // });
+    success = false;
+    comp.googleAttachSignIn(dummyElement)
+      .catch((error) => expect(error).toBe(errormsg));
+  });
 
-  // it('-googleSignOut() should call googleService.signOut', async(() => {
-  //   spyOn(googleService, 'signOut');
-  //   component.googleSignOut();
+  it('-googleSignOut() should call googleService.signOut', async(() => {
+    success = true;
+    comp.googleSignOut()
+      .then(() => expect(auth2).toBe(null));
 
-  //   fixture.whenStable().then(() => expect(googleService.signOut).toHaveBeenCalled());
-  // }));
+    success = false;
+    comp.googleSignOut()
+      .catch((error) => expect(error).toBe(errormsg));
+  }));
 });
